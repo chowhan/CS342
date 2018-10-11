e43b5ac03d109e152e47fe9a920f5d567043fdc5
chowhan <chowhan@utexas.edu>
2018-10-11 13:51:52 -0500

    commit yaar

--- homework_06/homework/models.py
+++ homework_06/homework/models.py
@@ -27,33 +27,11 @@ class Block(nn.Module):
         #     nn.Conv2d(128, 6, 5, 2, 1),
         #     nn.AvgPool2d(3)
         # )
-
-        self.conv1 = nn.Conv2d(in_channel, out_channel, 3, stride, 1)
-        self.bn1 = nn.BatchNorm2d(out_channel)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channel, out_channel, 3, stride, 1)
-        self.bn2 = nn.BatchNorm2d(out_channel)
-        self.conv3 = nn.Conv2d(in_channel, out_channel, 1, stride, 1)
-        self.bn3 = nn.BatchNorm2d(in_channel)
-        self.stride = stride
     
     def forward(self, x):
         '''
         Your code here
         '''
-        res = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.stride != 1:
-            res = self.conv3(x)
-        out += res
-        x = self.relu(out)
         return x
         
     
@@ -69,16 +47,21 @@ class ConvNetModel(nn.Module):
         '''
         Your code here
         '''
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = Block(32, 0, 64, 1)
-        self.layer2 = Block(32, 0, 128, 2)
-        self.layer3 = Block(32, 0, 256, 2)
-        self.layer4 = Block(32, 0, 512, 2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc1 = nn.Linear(512, 6)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        # You can wrap all the layers using the nn.Sequential which will make your
+
+        self.res_block = nn.Sequential(
+            nn.Conv2d(6, 6, 1, 1, 0),
+            nn.ReLU(True),
+            nn.Conv2d(6, 6, 3, 1, 1),
+            nn.ReLU(True),
+            nn.Conv2d(6, 6, 1, 1, 0),
+            nn.ReLU(True),
+        )
+
+        self.conv2 = nn.Conv2d(6, 12, 5)
+        self.pool = nn.MaxPool2d(2)
+        self.linear = nn.Linear(9408, 6)
     
     def forward(self, x):
         '''
@@ -88,17 +71,9 @@ class ConvNetModel(nn.Module):
         '''
         
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
+        h = self.res_block(x)
+        x = self.conv2(h + x)
+        x = self.pool(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
+        x = self.linear(x)
         return x
