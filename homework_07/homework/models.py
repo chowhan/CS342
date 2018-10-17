66f86f9ad87c6d4e224e4b20de9e4e9cf8f4a02f
chowhan <chowhan@utexas.edu>
2018-10-17 00:08:30 -0500

    hw7

---
+++ homework_07/homework/models.py
@@ -0,0 +1,30 @@
+from torch import nn
+
+class FConvNetModel(nn.Module):
+
+	"""
+	Define your fully convolutional network here
+	"""
+
+	def __init__(self):
+		
+		super().__init__()
+		
+		'''
+		Your code here
+		'''
+		self.conv1 = nn.Conv2d(3, 6, 3, 2, 1)
+		self.conv2 = nn.Conv2d(6, 12, 3, 2)
+		self.upconv1 = nn.ConvTranspose2d(12, 6, 3, 2)
+		self.upconv2 = nn.ConvTranspose2d(6, 4, 3, 2, 1)
+
+	def forward(self, x):
+		
+		'''
+		Your code here
+		'''
+		c1 = self.conv1(x)
+		c2 = self.conv2(c1)
+		up1 = self.upconv1(c2)
+		up2 = self.upconv2(up1 + c1)
+		return up2
