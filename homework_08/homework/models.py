0615dde8b3ea0781fa76f39a64919c74f6354a93
chowhan <chowhan@utexas.edu>
2018-10-25 12:39:55 -0500

    c

--- homework_08/homework/models.py
+++ homework_08/homework/models.py
@@ -105,8 +105,8 @@ class FConvNetModel(nn.Module):
 
 		u1 = self.upconv2(c44 + c4)
 		u2 = self.upconv3(u1 + c3 + c33)
-		u3 = self.upconv4(u2 + c2)
-		u4 = self.upconv5(u3 + c1)
+		u3 = self.upconv4(u2 + c2 + c22)
+		u4 = self.upconv5(u3 + c1 + c11)
 		# u5 = self.upconv5(u4 + c1)
 
 		return u4
