308c9f8b128823ac917f413b7e9dd6141d3289a2
chowhan <chowhan@utexas.edu>
2018-10-25 18:44:46 -0500

    c

--- homework_08/homework/models.py
+++ homework_08/homework/models.py
@@ -23,7 +23,7 @@ class FConvNetModel(nn.Module):
 		Your code here
 		'''
 
-		self.conv1 = nn.Conv2d(4, 32, 5, 2, 2)
+		self.conv1 = nn.Conv2d(1, 32, 5, 2, 2)
 		self.bn1 = nn.BatchNorm2d(32)
 		self.conv2 = nn.Conv2d(32, 64, 5, 2, 2)
 		self.bn2 = nn.BatchNorm2d(64)
@@ -49,7 +49,7 @@ class FConvNetModel(nn.Module):
 
 		# print(labels.size())
 
-		c1 = self.conv1(x)
+		c1 = self.conv1(labels)
 		c1 = self.bn1(c1)
 		c1 = self.relu(c1)
 
