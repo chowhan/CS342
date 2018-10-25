dbe6039d17923de76dc3f805c8c1508ae18a1c5d
chowhan <chowhan@utexas.edu>
2018-10-25 11:52:19 -0500

    c

--- homework_08/homework/models.py
+++ homework_08/homework/models.py
@@ -103,13 +103,16 @@ class FConvNetModel(nn.Module):
 		# c5 = self.relu(c5)
 		c5 = self.conv5(image)
 
-		# print(c5.size())
-		# print(c4.size())
+
 
 		u1 = self.upconv2(c44 + c4)
 		u2 = self.upconv3(u1 + c3 + c33)
 		u3 = self.upconv4(u2 + c2 + c22)
 		u4 = self.upconv5(u3 + c1 + c11)
 		# u5 = self.upconv5(u4 + c1)
+
+		print(u1.size())
+		print(c3.size())
+		print(c33.size())
 
 		return u4
