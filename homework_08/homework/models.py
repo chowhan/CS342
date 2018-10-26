317beb9aa7ce4014313306efd46c7f8099e06bd1
chowhan <chowhan@utexas.edu>
2018-10-25 21:50:38 -0500

    c

--- homework_08/homework/models.py
+++ homework_08/homework/models.py
@@ -45,7 +45,7 @@ class FConvNetModel(nn.Module):
 		'''
 		Your code here
 		'''
-		hr_image = nn.functional.interpolate(image, scale_factor=4, mode='nearest')
+		hr_image = nn.functional.interpolate(image, scale_factor=4, mode='bilinear')
 		labels = one_hot(labels)
 		#labels = torch.unsqueeze(labels, 1).float()
 		x = torch.cat((hr_image, labels), 1)
