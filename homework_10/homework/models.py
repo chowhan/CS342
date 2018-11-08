f6399df3ce2eaf56c9eccc6aba7440bbacd16a60
Your Name <chowhan@utexas.edu>
2018-11-07 19:51:44 -0600

    c

--- homework_10/homework/models.py
+++ homework_10/homework/models.py
@@ -50,8 +50,9 @@ class Model(nn.Module):
 		self.conv1 = nn.Conv2d(3 , 32 , ks, 4, ks//2)
 		self.bn1 = nn.BatchNorm2d(32)
 		self.conv2 = nn.Conv2d(32, 64, ks, 4, ks//2)
-		self.bn1 = nn.BatchNorm2d(32)
+		self.bn2 = nn.BatchNorm2d(64)
 		self.conv3 = nn.Conv2d(64, 128 , ks, 4, ks//2)
+		self.bn3 = nn.BatchNorm2d(128)
 
 		self.lstm_layer = nn.RNN(
 				input_size = 128,
