1225a7e5a3bdb89ff3d384c360fbb8309310cc5e
Prithvi Chowhan <chowhan@cs.utexas.edu>
2018-11-09 12:34:21 -0600

    c

--- homework_10/homework/models.py
+++ homework_10/homework/models.py
@@ -81,7 +81,7 @@ class Model(nn.Module):
 
 		x = x.view(batch_size, sequence_length, -1)
 		x = self.relu(self.linear1(x))
-		x = self.relu(self.linear2(x))
+		x = self.linear2(x)
 		x = x.permute(0, 2, 1)
 		x = F.pad(x, (self.width-1,0))
 		x = self.model(x)
