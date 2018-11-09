017da892686153792fb53c71f5fd677f4bf0009d
Prithvi Chowhan <chowhan@cs.utexas.edu>
2018-11-08 19:28:58 -0600

    c

--- homework_10/homework/models.py
+++ homework_10/homework/models.py
@@ -57,11 +57,11 @@ class Model(nn.Module):
 		self.hidden_dim = 64
 
 		ks = 5
-		self.conv1 = nn.Conv2d(3 , 32 , ks, 4, ks//2)
+		self.conv1 = nn.Conv2d(3 , 32 , ks, 3, ks//2)
 		self.bn1 = nn.BatchNorm2d(32)
-		self.conv2 = nn.Conv2d(32, 64, ks, 4, ks//2)
+		self.conv2 = nn.Conv2d(32, 64, ks, 3, ks//2)
 		self.bn2 = nn.BatchNorm2d(64)
-		self.conv3 = nn.Conv2d(64, 128 , ks, 4, ks//2)
+		self.conv3 = nn.Conv2d(64, 128 , ks, 3, ks//2)
 		self.bn3 = nn.BatchNorm2d(128)
 
 		self.linear1 = nn.Linear(128, 32)
